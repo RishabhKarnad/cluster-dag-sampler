84eb6352f681c3124f25cec0bd10b1361a447342
Rishabh Karnad <rishabhkarnad1995@gmail.com>
2023-08-22 20:17:58 +0530

    Fixes in greedy search

--- c_dag_info_theory.py
+++ c_dag_info_theory.py
@@ -2,6 +2,7 @@ from copy import deepcopy
 
 import numpy as np
 import scipy.stats as stats
+import scipy.linalg as linalg
 from tqdm import tqdm
 
 
@@ -26,29 +27,44 @@ def sample_cdag(C, rho=0.5):
     return G_C
 
 
+def h(G):
+    n, _ = G.shape
+    return linalg.expm(G*G).trace() - n
+
+
 def get_neighbours(G_C):
     C, E_C = G_C
     k = len(C)
     G = E_C
-    # G_clos = G.T
 
     neighbours = []
 
     # Add neighbours with one edge removed
     for i in range(k):
-        for j in range(i+1, k):
-            if G[i, j] == 1:
+        for j in range(k):
+            if i != j and G[i, j] == 1:
                 G_n = G.copy()
                 G_n[i, j] = 0
                 neighbours.append((deepcopy(C), G_n))
 
     # Add neighbours with one edge added
     for i in range(k):
-        for j in range(i+1, k):
-            if G[i, j] == 0:
+        for j in range(k):
+            if i != j and G[i, j] == 0:
                 G_n = G.copy()
                 G_n[i, j] = 1
                 neighbours.append((deepcopy(C), G_n))
+
+    # Add neighbours with one edge reversed
+    for i in range(k):
+        for j in range(k):
+            if i != j and G[i, j] == 1:
+                G_n = G.copy()
+                G_n[i, j] = 0
+                G_n[j, i] = 1
+                neighbours.append((deepcopy(C), G_n))
+
+    neighbours = list(filter(lambda n: h(n[1]) == 0, neighbours))
 
     return neighbours
 
@@ -66,6 +82,7 @@ def find_cdag(data, *, score, steps=100, rho=0.5):
     C_star = sample_clustering(n)
     G_star = sample_cdag(C_star, rho)
     score_G = score(G_star)
+    scores = []
 
     for _ in tqdm(range(steps)):
         C = sample_clustering(n)
@@ -75,21 +92,25 @@ def find_cdag(data, *, score, steps=100, rho=0.5):
         if score_new > score_G:
             G_star = G_prime
             score_G = score_new
+        scores.append(score_G)
 
-    return G_prime
+    return G_prime, scores
 
 
 def main():
     from scoreCIC import ScoreCIC
     from models.bernoulli import MultivariateBernoulliDistribution
     from data import generate_data_discrete
+    import matplotlib.pyplot as plt
 
-    data = generate_data_discrete(n_samples=100)
+    data = generate_data_discrete(n_samples=1000)
     score_CIC = ScoreCIC(
         data=data, dist=MultivariateBernoulliDistribution)
 
-    G_C = find_cdag(data, score=score_CIC)
+    G_C, scores = find_cdag(data, score=score_CIC, steps=10000)
     print(G_C)
+    plt.plot(scores)
+    plt.savefig('./scores.png')
 
 
 if __name__ == '__main__':
