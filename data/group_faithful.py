a1cfcb1cb1e038c5761dd6ec4d922c2de939a4ce
Rishabh Karnad <30865239+RishabhKarnad@users.noreply.github.com>
2023-12-21 23:07:10 +0530

    Add data loaders for group faithful DAG

---
+++ data/group_faithful.py
@@ -0,0 +1,134 @@
+import networkx as nx
+import numpy as np
+import random
+import itertools
+
+from generate_graphs import generate_graph
+
+random.seed(42)
+
+N = 10
+k = 3
+p = 0.2
+
+
+class GroupFaithfulDAG:
+    def get_cluster_matrix(self, W, N, n):
+        C = np.zeros((N, n))
+        for i, W_i in enumerate(W):
+            for w_i in list(W_i):
+                C[w_i, i] = 1
+        return C
+
+    def acyclicity(self, mat):
+        n_vars = mat.shape[0]
+        alpha = 1.0 / n_vars
+        # M = jnp.eye(n_vars) + alpha * mat * mat # [original version]
+        # since mat will be positive real matrix so double multiplication is not required
+        M = np.eye(n_vars) + alpha * mat
+
+        M_mult = np.linalg.matrix_power(M, n_vars)
+        h = np.trace(M_mult) - n_vars
+        return h
+
+    def conditional_independence_DAG(self, dag, groups, setA, setB, setC):
+        if len(setC) == 0:
+            cond_set = set()
+        else:
+            cond_set = set()
+            for i in range(len(setC)):
+                cond_set = cond_set.union(groups[list(setC)[i]])
+        for i in [groups[a] for a in list(setA)]:
+            for j in [groups[b] for b in list(setB)]:
+                # print(f'{i} {j} {cond_set}')
+                if not nx.d_separated(dag, i, j, cond_set):
+                    return False
+        return True
+
+    def get_group_dag_indeps(self, dag, W):
+        group_dag_nx = nx.from_numpy_array(dag, create_using=nx.MultiDiGraph())
+
+        n = len(W)
+
+        indeps = []
+
+        ord = 0
+        done = False
+        G = np.ones((n, n)) * (1 - np.eye(n))
+        G_nx = nx.from_numpy_array(G, create_using=nx.MultiDiGraph())
+        G_ = G.copy()
+
+        while not done:
+            done = True
+            indices = np.argwhere(G_ > 0)
+            X, Y = indices[:, 0].flatten(), indices[:, 1].flatten()
+            for i in range(X.size):
+                x = X[i]
+                y = Y[i]
+
+                nbrs = set(nx.neighbors(G_nx, y)).difference({x})
+
+                if len(nbrs) >= ord:
+                    done = False
+                    # SS = subsets1(nbrs, ord)
+                    SS = [set(comb)
+                          for comb in itertools.combinations(nbrs, ord)]
+                    for S in SS:
+                        if self.conditional_independence_DAG(group_dag_nx, W, {x}, {y}, S):
+                            indeps.append((x, y, S))
+                            G[x, y] = 0
+                            G[y, x] = 0
+            ord = ord + 1
+        return indeps
+
+    def hasSameIndepencies(self, H_indeps, W, G):
+        indeps = self.get_group_dag_indeps(G, W)
+        return len(indeps) == len(H_indeps), len(indeps)
+
+    def gen_group_faithful_dag(self, N, k, p):
+        n = int(np.ceil(N / k))
+
+        group_dag = generate_graph(n, p)
+
+        W = list(map(set, np.array_split(np.random.permutation(N), n)))
+
+        indices = [np.random.randint(len(W_i)) for W_i in W]
+        W_i = [list(W_i)[indices[i]] for i, W_i in enumerate(W)]
+
+        C = self.get_cluster_matrix(W, N, n)
+
+        G = np.zeros((N, N))
+        for edge in np.argwhere(group_dag > 0):
+            G[W_i[edge[0]], W_i[edge[1]]] = 1
+
+        group_names = list(map(lambda x: {x}, np.arange(group_dag.shape[0])))
+        H_indeps = self.get_group_dag_indeps(group_dag, group_names)
+
+        for _ in range(1000):
+            [u, v] = random.sample(range(N), 2)
+
+            G_prime = G.copy()
+
+            if G_prime[u, v] == 1:
+                G_prime[u, v] = 0
+            else:
+                G_prime[u, v] = 1
+
+            if self.acyclicity(G_prime) > 0:
+                continue
+
+            flag, length = self.hasSameIndepencies(H_indeps, W, G_prime)
+            if not flag:
+                continue
+
+            G = G_prime
+
+        return W, group_dag, G
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ data/loader.py
@@ -0,0 +1,263 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import igraph as ig
+from jax import random
+import jax.numpy as jnp
+from pgmpy.models import LinearGaussianBayesianNetwork
+from pgmpy.factors.continuous import LinearGaussianCPD
+import scipy
+
+
+class DataGen:
+    def __init__(self, key, obs_noise):
+        self.key = key
+        self.obs_noise = obs_noise
+        self.adjacency_matrix = None
+        self.theta = None
+        # self.vstruct=True
+        # self.faithful=False
+
+    def generate_data_continuous_5(self, n_samples=100, vstruct=True, faithful=True):
+        self.adjacency_matrix = np.zeros((5, 5))
+        self.vstruct = vstruct
+        self.faithful = faithful
+
+        if not self.vstruct:
+            print('graph no vstruct')
+            self.adjacency_matrix[0, 1] = 1
+            self.adjacency_matrix[0, 2] = 1
+            self.adjacency_matrix[1, 2] = 1
+            self.adjacency_matrix[1, 3] = 1
+            self.adjacency_matrix[3, 4] = 1
+        else:
+            # with vstructure
+            print('graph with vstruct')
+            self.adjacency_matrix[1, 0] = 1
+            self.adjacency_matrix[1, 2] = 1
+            self.adjacency_matrix[2, 0] = 1
+            self.adjacency_matrix[3, 2] = 1
+            self.adjacency_matrix[3, 4] = 1
+
+        G = nx.from_numpy_array(self.adjacency_matrix,
+                                create_using=nx.MultiDiGraph())
+        g = ig.Graph.from_networkx(G)
+        g.vs['label'] = g.vs['_nx_name']
+
+        nvars = len(g.vs)
+
+        self.key, subkey = random.split(self.key)
+        # random.normal(subkey,shape=(nvars,nvars))*100
+        self.theta = jnp.zeros((nvars, nvars))
+
+        if self.vstruct:
+            if self.faithful:
+                print('faithful with v structure')
+                self.theta = self.theta.at[1, 0].set(5)
+                self.theta = self.theta.at[2, 0].set(6)
+                self.theta = self.theta.at[0, 3].set(0)
+                self.theta = self.theta.at[2, 3].set(0)
+                self.theta = self.theta.at[1, 2].set(7)
+                self.theta = self.theta.at[3, 2].set(2)
+                self.theta = self.theta.at[3, 4].set(3)
+            else:
+                print('unfaithful with v structure')
+                self.theta = self.theta.at[1, 0].set(1)
+                self.theta = self.theta.at[2, 0].set(1)
+                self.theta = self.theta.at[0, 3].set(0)
+                self.theta = self.theta.at[2, 3].set(0)
+                self.theta = self.theta.at[1, 2].set(-1)
+                self.theta = self.theta.at[3, 2].set(2)
+                self.theta = self.theta.at[3, 4].set(3)
+        else:
+            if self.faithful:
+                print('faithful no vstructure')
+                self.theta = self.theta.at[0, 1].set(5)
+                self.theta = self.theta.at[0, 2].set(6)
+                self.theta = self.theta.at[0, 3].set(0)
+                self.theta = self.theta.at[2, 3].set(0)
+                self.theta = self.theta.at[1, 2].set(7)
+                self.theta = self.theta.at[1, 3].set(2)
+                self.theta = self.theta.at[3, 4].set(3)
+            else:
+                print('unfaithful without v structure')
+                self.theta = self.theta.at[0, 1].set(1)
+                self.theta = self.theta.at[0, 2].set(1)
+                self.theta = self.theta.at[0, 3].set(0)
+                self.theta = self.theta.at[2, 3].set(0)
+                self.theta = self.theta.at[1, 2].set(-1)
+                self.theta = self.theta.at[1, 3].set(2)
+                self.theta = self.theta.at[3, 4].set(3)
+
+        # self.theta=self.theta.at[0,1].set(3)
+        # self.theta=self.theta.at[0,2].set(-6)
+        # self.theta=self.theta.at[0,3].set(0)
+        # self.theta=self.theta.at[2,3].set(0)
+        # self.theta=self.theta.at[1,2].set(2)
+        # self.theta=self.theta.at[1,3].set(4)
+        # self.theta=self.theta.at[3,4].set(5)
+
+        noise = np.sqrt(self.obs_noise) * \
+            np.random.normal(size=(n_samples, nvars))
+
+        toporder = g.topological_sorting()
+
+        X = jnp.zeros((n_samples, nvars))
+
+        for j in toporder:
+            parent_edges = g.incident(j, mode='in')
+            parents = list(g.es[e].source for e in parent_edges)
+
+            if parents:
+                mean = X[:, jnp.array(
+                    parents)] @ self.theta[jnp.array(parents), j]
+                X = X.at[:, j].set(mean + noise[:, j])
+
+            else:
+                X = X.at[:, j].set(noise[:, j])
+
+        return X, (self.adjacency_matrix, self.theta, self.compute_fullcov())
+
+    def generate_data_continuous_3(self, theta, Cov, n_samples=100):
+        self.adjacency_matrix = np.zeros((3, 3))
+        self.adjacency_matrix[0, 1] = 1
+        self.adjacency_matrix[0, 2] = 1
+        self.adjacency_matrix[1, 2] = 1
+
+        G = nx.from_numpy_array(self.adjacency_matrix,
+                                create_using=nx.MultiDiGraph())
+        g = ig.Graph.from_networkx(G)
+        g.vs['label'] = g.vs['_nx_name']
+
+        nvars = len(g.vs)
+
+        self.key, subkey = random.split(self.key)
+        # self.theta=random.normal(subkey,shape=(nvars,nvars))*1
+        # self.theta=self.theta.at[0,1].set(1)
+        # self.theta=self.theta.at[0,2].set(1)
+        # self.theta=self.theta.at[1,2].set(-1)
+        # self.theta=np.array([[0,1,1],[1,0,-1],[1,-1,0]])
+        # self.theta=np.array([[0,1,1],[1,0,2],[1/2,1/2,0]])
+        self.theta = theta
+        self.Cov = Cov
+
+        # noise = np.sqrt(self.obs_noise)*np.random.normal(size=(n_samples, nvars))
+        n1 = np.sqrt(Cov[0, 0])*np.random.normal(size=(n_samples, 1))
+        n2 = np.sqrt(Cov[1, 1])*np.random.normal(size=(n_samples, 1))
+        n3 = np.sqrt(Cov[2, 2])*np.random.normal(size=(n_samples, 1))
+        noise = np.concatenate((n1, n2, n3), axis=1)
+
+        toporder = g.topological_sorting()
+
+        X = jnp.zeros((n_samples, nvars))
+
+        for j in toporder:
+            parent_edges = g.incident(j, mode='in')
+            parents = list(g.es[e].source for e in parent_edges)
+
+            if parents:
+                mean = X[:, jnp.array(
+                    parents)] @ self.theta[jnp.array(parents), j]
+                X = X.at[:, j].set(mean + noise[:, j])
+
+            else:
+                X = X.at[:, j].set(noise[:, j])
+
+        return X, self.theta
+
+    def obtain_truecluster(self):
+        """Define cluster"""
+        C = np.zeros((5, 3))
+        C[0:3, 0] = 1
+        C[3, 1] = 1
+        C[4, 2] = 1
+        return C
+
+    def obtain_truegraph(self):
+        Gc = np.zeros((3, 3))
+        Gc[0, 1] = 1
+        Gc[1, 2] = 1
+        return Gc
+
+    def viz_graph(adjacency_matrix, graph_lib='igraph'):
+        G = nx.from_numpy_array(
+            adjacency_matrix, create_using=nx.MultiDiGraph())
+        g = ig.Graph.from_networkx(G)
+        g.vs['label'] = g.vs['_nx_name']
+
+        if graph_lib == 'igraph':
+            fig, ax = plt.subplots()
+            ig.plot(g, target=ax)
+        elif graph_lib == 'nx':
+            nx.draw_circular(G, with_labels=True)
+
+    def compute_fullcov(self):
+        G = nx.from_numpy_array(self.adjacency_matrix,
+                                create_using=nx.MultiDiGraph())
+        G_LBN = nx.from_numpy_array(
+            self.adjacency_matrix, create_using=LinearGaussianBayesianNetwork)
+
+        factors = []
+        for node in G_LBN.nodes:
+            print(node)
+            parents = list(G.predecessors(node))
+            if parents:
+                theta_ = [0.0]
+                theta_ += self.theta[jnp.array(parents), node].tolist()
+                print(theta_)
+                factor = LinearGaussianCPD(
+                    node, theta_, self.obs_noise, parents)
+            else:
+                print('here')
+                factor = LinearGaussianCPD(
+                    node, [0.0], self.obs_noise, parents)
+            factors.append(factor)
+
+        G_LBN.add_cpds(*factors)
+        joint_dist = G_LBN.to_joint_gaussian()
+        Cov_true = joint_dist.covariance
+        # print(joint_dist.mean)
+        # print(Cov_true)
+        return Cov_true
+
+    def compute_jointcov(self):
+        G = nx.from_numpy_array(self.adjacency_matrix,
+                                create_using=nx.MultiDiGraph())
+        G_LBN = nx.from_numpy_array(
+            self.adjacency_matrix, create_using=LinearGaussianBayesianNetwork)
+
+        factors = []
+        for node in G_LBN.nodes:
+            print(node)
+            parents = list(G.predecessors(node))
+            if parents:
+                theta_ = [0.0]
+                theta_ += self.theta[jnp.array(parents), node].tolist()
+                print(theta_)
+                factor = LinearGaussianCPD(
+                    node, theta_, self.Cov[node, node], parents)
+            else:
+                print('here')
+                factor = LinearGaussianCPD(
+                    node, [0.0], self.Cov[node, node], parents)
+            factors.append(factor)
+
+        G_LBN.add_cpds(*factors)
+        joint_dist = G_LBN.to_joint_gaussian()
+        Cov_true = joint_dist.covariance
+        # print(joint_dist.mean)
+        # print(Cov_true)
+        return Cov_true
+
+    def compute_noisycov(self, C):
+        # C:nvarsXnclus
+        nvars, nclus = C.shape
+        Cov_true = self.compute_fullcov()
+        G_cov = C@C.T
+        G_anticov = np.ones((nvars, nvars))-G_cov
+        Cov_mask_true = G_cov*Cov_true
+        Cov_noise = scipy.stats.wishart.rvs(nvars, 100, size=(5, 5))
+        Cov_mask_true_noise = Cov_mask_true+G_anticov*Cov_noise
+        # print(Cov_mask_true)
+        # print(Cov_mask_true_noise)
+        return Cov_mask_true_noise, Cov_mask_true
